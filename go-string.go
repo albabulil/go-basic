248b48d5dd4ad6726bfa815eebac1ffee77de92a
ulil <cahok30@gmail.com>
2022-12-27 14:04:07 +0700

    go-basic | string

---
+++ go-string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ulil")
+	fmt.Println("Ulil Al")
+	fmt.Println("Ulil Albab")
+
+	fmt.Println("\n")
+
+	fmt.Println(len("Ulil"))     // menghitung jumlah karakter si string
+	fmt.Println("Ulil al"[0])    // mengambil karakter pada posisi yang di tentukan (di mulai dari 0) result byte
+	fmt.Println("ulil Albab"[0]) // mengambil karakter pada posisi yang di tentukan (di mulai dari 0) result byte
+}
